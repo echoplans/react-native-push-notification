59eb105e8272173481a9b84dfe4cf10ecfaf7455
Srijan Gupta <srijan02420@gmail.com>
2017-07-31 17:19:33 +0530

    Update RNPushNotificationHelper.java

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.media.RingtoneManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
